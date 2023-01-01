3e258c7ffc5473d0a9c81118c0f77a91f2de8377
TatoSec <ivang.fonsecafrias@gmail.com>
2022-12-31 23:35:33 -0500

    Updated App

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Devops Project
+          Github link!
         </a>
       </header>
     </div>

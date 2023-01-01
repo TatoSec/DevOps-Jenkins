ff9b00336f77620edb5e2a2a1d167f0ee632d034
TatoSec <ivang.fonsecafrias@gmail.com>
2023-01-01 14:58:46 -0500

    App Test

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          GitHub Link
+          GitHub Link!
         </a>
       </header>
     </div>

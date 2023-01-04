ecae7b3720c22407b1604876bb7915a9338d3aaf
TatoSec <ivang.fonsecafrias@gmail.com>
2023-01-04 16:37:12 -0500

    Portfolio Push

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import './App.module.scss';
+import BaseLayout from "./components/BaseLayout";
+import {BrowserRouter} from "react-router-dom";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1>
-          Devops Project
-        </h1>
-        <a
-          className="App-link"
-          href="https://github.com/TatoSec/DevOps-Jenkins"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          github!
-        </a>
-      </header>
-    </div>
-  );
+   return (
+      <div>
+         <BrowserRouter>
+            <BaseLayout/>
+         </BrowserRouter>
+      </div>
+   );
 }
+
 
 export default App;

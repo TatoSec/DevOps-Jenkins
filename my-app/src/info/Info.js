6f1cc39ba36c7d19c8d81fed5f7e0345359dd2b0
TatoSec <ivang.fonsecafrias@gmail.com>
2023-01-05 11:19:19 -0500

    Apps Updated

--- my-app/src/info/Info.js
+++ my-app/src/info/Info.js
@@ -95,13 +95,13 @@ export const info = {
     ],
     portfolio: [ // This is where your portfolio projects will be detailed
         {
-            title: "Project 1",
+            title: "Project: Jenkins-DevOps",
             live: "https://paytonpierce.dev", //this should be a link to the live version of your project, think github pages, netlify, heroku, etc. Or your own domain, if you have it.
-            source: "https://github.com/paytonjewell", // this should be a link to the **repository** of the project, where the code is hosted.
+            source: "https://github.com/TatoSec/DevOps-Jenkins", // this should be a link to the **repository** of the project, where the code is hosted.
             image: mock1
         },
         {
-            title: "Project 2",
+            title: "Project: ",
             live: "https://paytonpierce.dev",
             source: "https://github.com/paytonjewell",
             image: mock2
